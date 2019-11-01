434e3dd6cfac9520679c1dd9ce4c038483406b96
Hari Arunachalam <hr695936@dal.ca>
2019-11-01 19:59:31 -0300

    First Commit. -Splash screen

---
+++ ProjectWorkspace/app/src/main/java/com/example/spotquiz/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.spotquiz;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.net.ContentHandlerFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+
+        setContentView(R.layout.activity_main);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+            Intent i = new Intent(MainActivity.this,LoginActivity.class);
+            startActivity(i);
+        //close
+
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+
+
+    }
+}
