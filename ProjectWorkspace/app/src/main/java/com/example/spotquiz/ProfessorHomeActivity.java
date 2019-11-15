5b596d8e54c3f05e13cddbcd4a202a62e5e1773f
pish <hariarun.19@gmail.com>
2019-11-15 11:28:44 -0800

    Login material corrections

---
+++ ProjectWorkspace/app/src/main/java/com/example/spotquiz/ProfessorHomeActivity.java
@@ -0,0 +1,14 @@
+package com.example.spotquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfessorHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_professor_home);
+    }
+}
---
+++ ProjectWorkspace/app/src/main/java/com/example/spotquiz/QuizCreationActivity.java
@@ -0,0 +1,14 @@
+package com.example.spotquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuizCreationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_creation);
+    }
+}
