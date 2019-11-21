f13417ba1f9452a9b7f75d0abe11f65adc1ce5a6
Hari Arunachalam <hr695936@dal.ca>
2019-11-21 14:57:39 -0400

    Revert "Merge branch 'studenthome' into 'master'"
    This reverts merge request !2

--- ProjectWorkspace/app/src/main/java/com/example/spotquiz/StudentHomeActivity.java
+++ ProjectWorkspace/app/src/main/java/com/example/spotquiz/StudentHomeActivity.java
@@ -1,64 +1,24 @@
 package com.example.spotquiz;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
 
 public class StudentHomeActivity extends AppCompatActivity {
-
-    private ListView listView;
-    private  Button btnquiz;
-    private DatabaseReference database;
+Button logout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_home);
 
+        logout = findViewById(R.id.btnLogout);
 
-        listView = (ListView) findViewById(R.id.list);
-        database = FirebaseDatabase.getInstance().getReference("Quizes");
-        final ArrayList<String> arrayList = new ArrayList<>();
-
-        database.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                arrayList.clear();
-                for(DataSnapshot child : dataSnapshot.getChildren()){
-                    String quiz = child.getValue(String.class);
-                    arrayList.add(quiz);
-                }
-                ArrayAdapter arrayAdapter = new ArrayAdapter(StudentHomeActivity.this, android.R.layout.simple_list_item_1,arrayList);
-                listView.setAdapter(arrayAdapter);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-
-
-
-
-
-        btnquiz = findViewById(R.id.btnquiz);
-
-        btnquiz.setOnClickListener(new View.OnClickListener() {
+        logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 FirebaseAuth.getInstance().signOut();
